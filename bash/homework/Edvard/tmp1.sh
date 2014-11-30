1151a569ab88f10cc65059d223f842bfd68992ab
Eduard Hakobyan <edtennis@yandex.com>
2014-12-01 02:03:39 +0400

    Homework N4

---
+++ bash/homework/Edvard/tmp1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rm -rf ITC 
+mkdir FORITC
+sudo chmod 777 FORITC
+
+
+
+
+
+#որպ եսզի սկրիպ տ ը ա վտ ոմա տ   միա նա  սա րքում ենք /etc/rc.local ֆա յլ և ա յնտ եղ  տ եղա դրում հետ և յա լը․
+
+
+
+##!/bin/bash
+
+#նշում ենք սկրիպ տ ի տ եղը /․․/․․/tmp.sh
+#exit 0
+
+# Վ Ե Ր Ջ
+
